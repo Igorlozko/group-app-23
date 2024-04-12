7fbc9573fec82ea90de58c7b3d6e2eb4edd2476e
PATRICK HESSION <G00394619@gmit.ie>
2024-04-12 03:13:05 +0100

    Add react-native-elements dependency and update UI in Review.jsx

--- App/Screen/Reviews/Review.jsx
+++ App/Screen/Reviews/Review.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TextInput, Button, FlatList } from 'react-native';
-
+import { View, Text, Image, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { Card } from 'react-native-elements';
 function Review({route}) {
   const { imageUrl } = route.params;
   const [review, setReview] = useState('');
@@ -9,11 +9,19 @@ function Review({route}) {
   useEffect(() => {
     fetch('https://c0d3-79-140-211-73.ngrok-free.app/reviews')
       .then(response => response.json())
-      .then(data => setReviews(data))
+      .then(data => {
+        console.log('Received data:', data);
+        setReviews(data);
+      })
       .catch(error => console.error('Error:', error));
   }, []);
-
   const submitReview = async () => {
+    // If the review is empty, return early to prevent the fetch request
+    if (!review.trim()) {
+      console.log('Review is empty');
+      return;
+    }
+  
     const response = await fetch('https://c0d3-79-140-211-73.ngrok-free.app/reviews', {
       method: 'POST',
       headers: {
@@ -38,23 +46,43 @@ function Review({route}) {
     <View>
       {imageUrl && <Image source={{ uri: imageUrl }} style={{ width: '100%', height: 200 }} />}
       <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        placeholder="Write a Note"
-        onChangeText={text => setReview(text)}
-        value={review}
-      />
-      <Button title="Submit Review" onPress={submitReview} />
-
+  style={{
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10, // Add some padding to the left of the text
+    borderRadius: 5, // Round the corners
+    margin: 10, // Add some margin around the input
+  }}
+  placeholder="Write a Note"
+  onChangeText={text => setReview(text)}
+  value={review}
+/>
+<TouchableOpacity style={{
+  backgroundColor: 'white',
+  padding: 10,
+  borderRadius: 5,
+  alignItems: 'center',
+  margin: 10,
+  borderColor: 'black', // Add a black border
+  borderWidth: 1, // Set the border width
+}} onPress={submitReview}>
+  <Text>Add a Note</Text>
+</TouchableOpacity>
       <FlatList
         data={reviews}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.name}</Text>
+          <Card>
+            <Card.Title>{item.name}</Card.Title>
+            <Card.Divider/>
             <Text>{item.description}</Text>
-          </View>
+          </Card>
         )}
       />
     </View>
   );
 }
+
+
+export default Review;
